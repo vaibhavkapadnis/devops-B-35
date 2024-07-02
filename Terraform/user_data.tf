fd4bffe06201ee60181d4856d64d1a138bf9d68e
abhipraydhoble <122669982+abhipraydhoble@users.noreply.github.com>
2024-07-02 11:34:55 +0530

    Update user_data.tf

--- Terraform/user_data.tf
+++ Terraform/user_data.tf
@@ -1,3 +1,4 @@
+# User_Data Example 1
 resource "aws_instance" "vm" {
   ami           = "ami-01376101673c89611"
   instance_type = "t2.micro"
@@ -17,6 +18,23 @@ resource "aws_instance" "vm" {
   tags = {
     Name = "Apache-Server"
   }
+}
+
+---
+# User_Data Example 1
+resource "aws_instance" "vm" {
+  ami           = "ami-01376101673c89611"
+  instance_type = "t2.micro"
+  key_name      = "tf-key"
+  user_data = file("script.sh")
+  tags = {
+    Name = "HTTPD-Server"
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.vm.public_ip
+  
 }
 
 output "public_ip" {
