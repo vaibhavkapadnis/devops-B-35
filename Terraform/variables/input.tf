51005b5856e545c195d83d82ea82eb1678fe610b
abhipraydhoble <122669982+abhipraydhoble@users.noreply.github.com>
2024-07-04 16:45:24 +0530

    Create input.tf

---
+++ Terraform/variables/input.tf
@@ -0,0 +1,18 @@
+variable "ami_id" {
+  type        = string
+  description = "assign ami id"
+  
+}
+
+variable "instance_type" {
+  type        = string
+  description = "use this instance type"
+  
+
+}
+
+variable "key_name" {
+  type    = string
+  
+
+}
