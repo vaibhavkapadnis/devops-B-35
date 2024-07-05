06bae1ea94c57b06803627edc5911beb16138418
abhipraydhoble <122669982+abhipraydhoble@users.noreply.github.com>
2024-07-05 11:32:15 +0530

    Update input.tf

--- Terraform/variables/input.tf
+++ Terraform/variables/input.tf
@@ -16,3 +16,24 @@ variable "key_name" {
   
 
 }
+
+variable "vpc_cidr_block" {
+  type = string
+
+}
+
+variable "subnet_block" {
+  type = list(string)
+  
+}
+
+variable "public_ip" {
+  type = bool
+
+}
+
+variable "az" {
+  type = list(string)
+
+  
+}
